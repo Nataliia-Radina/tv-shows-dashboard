cf98128c8abd61d6dfab2a0008475eab82f42e06
Nataliia-Radina <shyianovska@gmail.com>
2025-03-24 21:00:47 +0100

    fix: update router to use correct base URL for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 import ShowDetails from '../views/ShowDetails.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
